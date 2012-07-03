76e9907588f18a2cf70ba81d8b6e1fe956afc8b7
Alex Kalderimis <alex@flymine.org>
2012-07-03 09:37:20 +0100

    Fix path alias retention issue on graph navigation

--- build/im.js
+++ build/im.js
@@ -191,7 +191,7 @@
         if (this.isRoot()) {
             throw "Root paths do not have a parent";
         }
-        var data = _.extend({}, this, {descriptors: _(this.descriptors).initial()});
+        var data = _.extend({}, this, {descriptors: _(this.descriptors).initial(), displayName: null});
         return new PathInfo(data);
     };
 
@@ -200,7 +200,7 @@
         if (_(attr).isString()) {
             attr = type.fields[attr];
         }
-        var data = _.extend({}, this, {descriptors: this.descriptors.concat([attr])});
+        var data = _.extend({}, this, {descriptors: this.descriptors.concat([attr]), displayName: null});
         return new PathInfo(data);
     };
 
