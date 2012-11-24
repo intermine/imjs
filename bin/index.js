0d3e38aa833522a2ce2158d87c7b45091ca1b76a
Alex Kalderimis <alex.kalderimis@gmail.com>
2012-11-24 13:15:42 +0000

    Added index file to re-export code under node.

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+var m = require('../build/model'),
+    q = require('../build/query'),
+    s = require('../build/service');
+
+exports.Model = m.Model;
+exports.Query = q.Query;
+exports.Service = s.Service;
