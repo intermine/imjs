baaca71d536303918b15be9f2257f42fbe141afd
Alex Kalderimis <alexkalderimis@gmail.com>
2012-11-30 18:24:55 +0100

    Code clean up, and switch to standard node.js style

--- tasks/build-qunit-index.js
+++ tasks/build-qunit-index.js
@@ -15,59 +15,68 @@ module.exports = function (grunt) {
     var template = grunt.template;
 
     function writeUnified (data, conf, done) {
-        var compiled = template.process(data, conf);
-        var dest = template.process(conf.dest, {idx: 0, file: 'all'})
-        var written = grunt.file.write(dest, compiled);
-        if (written) {
-            grunt.log.ok("Wrote " + dest);
-        } else {
-            grunt.log.error("Failed to write " + dest);
-        }
-        done(written);
+      var html, dest, written = false;
+      try {
+        compiled = template.process(data, conf);
+        dest     = template.process(conf.dest, {idx: 0, file: 'all'})
+        written  = grunt.file.write(dest, compiled);
+        grunt.log.ok("Wrote " + dest);
+      } catch (e) {
+        grunt.log.error("Failed to write html");
+      }
+      done(written);
     }
 
     function writeSeparate (data, conf, done) {
-        _.each(conf.testFiles, function handleFile(tf, idx) {
-            var file     = path.basename(tf);
-            var destCtx  = {idx: idx, file: file};
-            var templCtx = _.defaults({}, {testFiles: [tf]}, conf);
-            var dest     = template.process(conf.dest, destCtx);
-            var compiled = template.process(data, templCtx);
-            grunt.verbose.writeln("Writing " + dest);
-            return grunt.file.write(dest, compiled);
-        });
-        grunt.log.ok("Wrote " + conf.testFiles.length + " files");
-        done(true);
+      var tf, idx, uri, fileName, destFile, html;
+      var files = conf.testFiles;
+      var filesL = files.length;
+      var fileName, destFile, html;
+      for (idx = 0; idx < filesL; idx++) {
+        uri      = files[idx];
+        fileName = path.basename(uri);
+        destFile = template.process(conf.dest, {idx: idx, file: fileName});
+        html     = template.process(data, _.defaults({testFiles: [uri]}, conf));
+        try {
+          grunt.file.write(destFile, html);
+          grunt.verbose.writeln("Wrote " + destFile);
+        } catch (e) {
+          grunt.log.error('Failed to write ' + destFile);
+          return done(false);
+        }
+      }
+      grunt.log.ok("Wrote " + filesL + " html files");
+      return done(true);
     }
 
     function getHandler (conf, done) {
-        var write = conf.unified ? writeUnified : writeSeparate;
+      var write = conf.unified ? writeUnified : writeSeparate;
 
-        return function handler(err, data) {
-            if (err) {
-                grunt.log.error('Could not read template file: ' + (err.stack || err));
-                done(false);
-            } else {
-                write(data, conf, done);
-            }
-        };
+      return function handler(err, data) {
+        if (err) {
+          grunt.log.error('Could not read template file: ' + (err.stack || err));
+          done(false);
+        } else {
+          write(data, conf, done);
+        }
+      };
     }
     
     grunt.registerTask(
-            'buildqunit',
-            'Build the QUnit index file',
-            function buildqunit() {
-        var done = this.async();
-        var conf = grunt.config('buildqunit');
-        var expandFileURLs = grunt.file.expandFileURLs;
+        'buildqunit',
+        'Build the QUnit index file',
+        function buildqunit() {
+      var done = this.async();
+      var conf = grunt.config('buildqunit');
+      var expandFileURLs = grunt.file.expandFileURLs;
 
-        conf.setup      = (conf.setup || '');
-        conf.dest       = (conf.dest || 'qunit-index.html');
-        conf.targets    = expandFileURLs(conf.tested);
-        conf.setupFiles = expandFileURLs(conf.setup);
-        conf.testFiles  =
-            _.difference(expandFileURLs(conf.tests), conf.setupFiles);
+      conf.setup      = (conf.setup || '');
+      conf.dest       = (conf.dest  || 'qunit-index.html');
+      conf.targets    = expandFileURLs(conf.tested);
+      conf.setupFiles = expandFileURLs(conf.setup);
+      conf.testFiles  =
+        _.difference(expandFileURLs(conf.tests), conf.setupFiles);
 
-        fs.readFile(conf.template, 'utf8', getHandler(conf, done));
+      fs.readFile(conf.template, 'utf8', getHandler(conf, done));
     });
 };
--- tasks/compile.js
+++ tasks/compile.js
@@ -2,93 +2,96 @@
  * Compile coffeescript source files.
  */
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    var fs = require('fs')
-      , path = require('path')
-      , coffee = require('coffee-script')
-    
+  var fs = require('fs');
+  var path = require('path');
+  var coffee = require('coffee-script');
 
-    var compile = function (src, dest, cb) {
-        var files = grunt.file.expandFiles([src])
-          , fileCount = files.length
-          , successes = 0
-          , error = false
-
-        if (fileCount < 1) {
-            return cb(new Error("No files matched '" + src + "'"));
-        }
-        var reportIfDone = function () {
-            if (error || successes >= fileCount) {
-                cb(error);
-            }
-        };
-
-        grunt.file.mkdir(dest);
-
-        files.forEach(function (file) {
-            if (file.match(/\.coffee$/)) {
-                fs.readFile(file, 'utf8', function (err, data) {
-                    if (err) {
-                        grunt.log.error("Could not read " + file);
-                        error = err;
-                    } else {
-                        var compiled
-                        , baseName = path.basename(file, path.extname(file))
-                        , newName = path.join(dest, baseName + '.js')
-                        try {
-                            compiled = coffee.compile(data);
-                        } catch (e) {
-                            grunt.log.error("Could not compile " + file);
-                            error = e;
-                            return reportIfDone();
-                        }
-
-                        fs.writeFile(newName, compiled, function (err) {
-                            if (err) {
-                                grunt.log.error("Could not write " + newName);
-                                error = err;
-                            } else {
-                                grunt.verbose.ok("Wrote " + newName);
-                                successes++;
-                            }
-                            reportIfDone();
-                        });
-                    }
-                    reportIfDone();
-                });
-            } else {
-                successes++;
-            }
-            reportIfDone();
-        });
-    };
-    
-    /**
-     * Compile a directory of source files to an output directory.
-     */
-    grunt.registerMultiTask('compile', 'Compile source files to js', function() {
-        var opts
-          , log   = grunt.log
-          , done  = this.async()
-          , src   = this.data.src
-          , dest  = this.data.dest
-
-        if (src && dest) {
-
-            compile(src, dest, function (err) {
-                if (err) {
-                    log.error("Compilation failed: " + (err.stack || err));
-                    done(false);
-                } else {
-                    log.ok('done: compiled coffee-script files in ' + src + ' to ' + dest + '.');
-                    done(true);
-                }
-            });
-
-        } else {
-            log.error("No source directory specified");
-            done(false);
-        }
+  var writeJS = function (dest, file, js, reportError) {
+    var baseName = path.basename(file, path.extname(file));
+    var newName = path.join(dest, baseName + '.js');
+    fs.writeFile(newName, js, function (e) {
+      if (e) {
+        grunt.log.error('Failed to write ' + newName);
+      } else {
+        grunt.verbose.ok('Wrote ' + newName);
+      }
+      return reportError(e);
     });
+  };
+
+  var compileFile = function (dest, file, reportError) {
+    fs.readFile(file, 'utf8', function (err, data) {
+      var compiled;
+      if (err) {
+        grunt.log.error("Could not read " + file);
+        return reportError(err);
+      } else {
+        try {
+          compiled = coffee.compile(data);
+        } catch (e) {
+          grunt.log.error("Could not compile " + file);
+          return reportError(e);
+        }
+
+        writeJS(dest, file, compiled, reportError);
+      }
+    });
+  };
+
+  var compile = function (src, dest, cb) {
+    var files     = grunt.file.expandFiles([src]);
+    var fileCount = files.length;
+    var successes = 0;
+
+    if (fileCount < 1) {
+      return cb(new Error("No files matched '" + src + "'"));
+    }
+
+    var finishedWithFile = function (error) {
+      successes += 1;
+      if (error || successes >= fileCount) {
+        cb(error);
+      }
+    };
+
+    // Ensure that the destination exists.
+    grunt.file.mkdir(dest);
+
+    var i = 0, file;
+    for (i = 0; file = files[i]; i++) {
+      if (file.match(/\.coffee$/)) {
+        compileFile(dest, file, finishedWithFile));
+      } else {
+        finishedWithFile()
+      }
+    }
+  };
+  
+  /**
+   * Compile a directory of source files to an output directory.
+   */
+  grunt.registerMultiTask('compile', 'Compile source files to js', function() {
+    var opts;
+    var log   = grunt.log;
+    var done  = this.async();
+    var src   = this.data.src;
+    var dest  = this.data.dest;
+
+    if (src && dest) {
+      compile(src, dest, function (err) {
+        if (err) {
+          log.error("Compilation failed: " + (err.stack || err));
+          done(false);
+        } else {
+          log.ok('done: compiled coffee-script files in ' + src + ' to ' + dest + '.');
+          done(true);
+        }
+      });
+    } else {
+      log.error("No source directory specified");
+      done(false);
+    }
+  });
 };
