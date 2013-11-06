bc8238c3ecebf760d47b3b4327861bcf3941f9c0
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-11-06 19:21:01 +0000

    Update version

--- js/im.js
+++ js/im.js
@@ -1,4 +1,4 @@
-/*! imjs - v2.7.0 - 2013-11-06 */
+/*! imjs - v2.8.0 - 2013-11-06 */
 
 /**
 This library is open source software according to the definition of the
@@ -34,7 +34,7 @@ Thu Jun 14 13:18:14 BST 2012
       imjs.VERSION = pkg.version;
     }
   } else {
-    imjs.VERSION = "2.7.0";
+    imjs.VERSION = "2.8.0";
   }
 
 }).call(this);
