793605405549ca8319a74fee0bb2fa3e42b11ed0
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-11-15 17:12:24 +0000

    Make grunt:bump work correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,6 +108,7 @@ module.exports = function (grunt) {
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json'],
         pushTo: 'origin'
       }
     },
