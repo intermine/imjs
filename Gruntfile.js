2f25b6568b0919439898951f733580558714a9ae
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-11-20 18:20:27 +0000

    Make sure we commit all files after a bump.

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,7 @@ module.exports = function (grunt) {
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
-        commitFiles: ['package.json', 'bower.json'],
+        commitFiles: ['-a'],
         pushTo: 'origin'
       }
     },
