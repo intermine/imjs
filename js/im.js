7b29bd2c280cc7f4d6a939b47bcf57314b1f3143
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-11-21 19:21:47 +0000

    Version bump

--- js/im.js
+++ js/im.js
@@ -1,4 +1,4 @@
-/*! imjs - v2.10.4 - 2013-11-21 */
+/*! imjs - v2.10.5 - 2013-11-21 */
 
 // This library is open source software according to the definition of the
 // GNU Lesser General Public Licence, Version 3, (LGPLv3) a copy of which is
@@ -29,7 +29,7 @@
       imjs.VERSION = pkg.version;
     }
   } else {
-    imjs.VERSION = "2.10.4";
+    imjs.VERSION = "2.10.5";
   }
 
 }).call(this);
