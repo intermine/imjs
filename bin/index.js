dfbb9dc49087bfb64ed00c2c7bcc88d23c52eb31
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-12-02 16:56:58 +0000

    Follow naming convention better

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-var m = require('../build/model');
-var q = require('../build/query');
-var s = require('../build/service');
-var v = require('../build/version');
-
-exports.Model = m.Model;
-exports.Query = q.Query;
-exports.Service = s.Service;
-exports.VERSION = v.VERSION;
