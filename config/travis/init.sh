df8ed28473eed6bab736ba454960bf4c06074dde
Alex Kalderimis <alex.kalderimis@gmail.com>
2014-11-26 15:39:49 +0000

    Travis build configuration.

---
+++ config/travis/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+if [ -z $(which wget) ]; then
+    # use curl
+    GET='curl'
+else
+    GET='wget -O -'
+fi
+
+# Pull in the server code.
+git clone https://github.com/intermine/intermine.git server
+
+# We need a running demo webapp
+source server/config/download_and_configure_tomcat.sh
+sleep 10 # wait for tomcat to come on line
+# Get messages from 500 errors.
+echo 'i.am.a.dev = true' >> server/testmodel/testmodel.properties
+PSQL_USER=postgres sh server/testmodel/setup.sh
+sleep 10 # wait for the webapp to come on line
+
+# Warm up the keyword search by requesting results, but ignoring the results
+$GET $TESTMODEL_URL/service/search > /dev/null
+# Start any list upgrades by poking the lists service.
+$GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null
+
