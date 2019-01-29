523e29a88a863589344ce0bde0f1c0e59c0a1aaa
Yo Yehudi <yochannah@gmail.com>
2019-01-29 16:59:21 +0000

    trying to make travis build again

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -13,14 +13,13 @@ fi
 git clone --single-branch --branch 'master' --depth 1 https://github.com/intermine/intermine.git server
 
 # We need a running demo webapp
-source server/config/download_and_configure_tomcat.sh
+source server/testmodel/setup.sh
 sleep 5 # wait for tomcat to come on line
 # Get messages from 500 errors.
 echo 'i.am.a.dev = true' >> server/testmodel/testmodel.properties
 PSQL_USER=postgres sh server/testmodel/setup.sh
 sleep 15 # wait for the webapp to come on line
 
-# Warm up the keyword search by requesting results, but ignoring the results
-$GET $TESTMODEL_URL/service/search > /dev/null
+
 # Start any list upgrades by poking the lists service.
 $GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null
