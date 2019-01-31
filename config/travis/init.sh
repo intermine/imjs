d3476673dcfa95a97246960ab2960822d9605b12
Yo Yehudi <yochannah@gmail.com>
2019-01-31 10:51:53 +0000

    I'm hoping I resolved the directory problems. Go go go Travis

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -12,18 +12,16 @@ fi
 cd $HOME
 
 # Pull in the server code.
-git clone --single-branch --branch 'dev' --depth 1 https://github.com/yochannah/intermine.git server
-ls server
-ls server/testmine/
-ls server/testmine/dbmodel
-ls server/testmine/dbmodel/resources
+git clone --single-branch --branch 'master' --depth 1 https://github.com/intermine/intermine.git server
+
+cd server/testmine
 
 # We need a running demo webapp
-source server/testmine/setup.sh
+source setup.sh
 sleep 5 # wait for tomcat to come on line
 # Get messages from 500 errors.
-echo 'i.am.a.dev = true' >> server/testmine/dbmodel/resources/testmodel.properties
-PSQL_USER=postgres sh server/testmine/setup.sh
+echo 'i.am.a.dev = true' >> dbmodel/resources/testmodel.properties
+PSQL_USER=postgres sh setup.sh
 sleep 15 # wait for the webapp to come on line
 
 
