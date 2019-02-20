4b8e7bbf5036bacdd97e5616e7789121e8adddf3
julie-sullivan <julie@flymine.org>
2019-02-20 15:20:20 +0000

    update to new gradle intermine build.

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -9,18 +9,23 @@ else
     GET='wget -O -'
 fi
 
+cd $HOME
+
 # Pull in the server code.
-git clone --single-branch --branch 'dev' --depth 1 https://github.com/yochannah/intermine.git server
+git clone --single-branch --branch 'master' --depth 1 https://github.com/intermine/intermine.git server
+
+export PSQL_USER=postgres
 
 # We need a running demo webapp
-source server/config/download_and_configure_tomcat.sh
-sleep 5 # wait for tomcat to come on line
-# Get messages from 500 errors.
-echo 'i.am.a.dev = true' >> server/testmodel/testmodel.properties
-PSQL_USER=postgres sh server/testmodel/setup.sh
-sleep 15 # wait for the webapp to come on line
+# Set up properties
+source server/config/create-ci-properties-files.sh
+
+# We will need a fully operational web-application
+echo '#---> Building and releasing web application to test against'
+(cd server/testmine && ./setup.sh)
+sleep 60 # let webapp startup
 
 # Warm up the keyword search by requesting results, but ignoring the results
-$GET $TESTMODEL_URL/service/search > /dev/null
-# Start any list upgrades by poking the lists service.
+$GET "$TESTMODEL_URL/service/search" > /dev/null
+# Start any list upgrades
 $GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null
