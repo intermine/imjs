35ac3b7b4f7f55c5e3c23f826c2e4fd10e6f8f87
Yo Yehudi <yochannah@gmail.com>
2019-02-28 13:52:22 +0000

    Revert "Merge branch 'master' into dev"
    This reverts commit 3e190a446d4615839fbc2c6c7c7e556406716914, reversing
    changes made to 35f7ba53166ccd21a82efe9fd38ea88f532485e4.

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -14,35 +14,25 @@ cd $HOME
 # Pull in the server code.
 git clone --single-branch --branch 'master' --depth 1 https://github.com/intermine/intermine.git server
 
-cd server
-
 export PSQL_USER=postgres
 
-# We need a running demo webapp
 # Set up properties
-source config/create-ci-properties-files.sh
+PROPDIR=$HOME/.intermine
+TESTMODEL_PROPS=$PROPDIR/testmodel.properties
+SED_SCRIPT='s/PSQL_USER/postgres/'
 
-# install everything first. we don't want to test what's in maven
-(cd plugin && ./gradlew install)
-(cd intermine && ./gradlew install)
-(cd bio && ./gradlew install)
-(cd bio/sources && ./gradlew install)
-(cd bio/postprocess && ./gradlew install)
+mkdir -p $PROPDIR
 
-# set up database for testing
-(cd intermine && ./gradlew createUnitTestDatabases)
+echo "#--- creating $TESTMODEL_PROPS"
+cp server/config/testmodel.properties $TESTMODEL_PROPS
+sed -i -e $SED_SCRIPT $TESTMODEL_PROPS
 
 # We will need a fully operational web-application
 echo '#---> Building and releasing web application to test against'
-(cd testmine && ./setup.sh)
-
+(cd server/testmine && ./setup.sh)
 sleep 60 # let webapp startup
 
 # Warm up the keyword search by requesting results, but ignoring the results
 $GET "$TESTMODEL_URL/service/search" > /dev/null
 # Start any list upgrades
 $GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null
-
-
-# Get messages from 500 errors.
-echo 'i.am.a.dev = true' >> dbmodel/resources/testmodel.properties
